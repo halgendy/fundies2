2faba719cbc333f05291af582170bbb50a726a57
Hazem Algendy <156970407+halgendy@users.noreply.github.com>
2024-10-30 19:28:29 -0400

    Update deques.java
    worked on relink and added throw exceptions in constructor but for some reason exception is being thrown

--- deques.java
+++ deques.java
@@ -6,15 +6,34 @@ import tester.*;
 abstract class ANode<T> {
   ANode<T> next;
   ANode<T> prev;
+  
+  //
+  abstract T remove();
+  
+  //
+  void relink(Node<T> nextLink, Node<T> prevLink) {
+    if (nextLink != null) {
+      this.next = prevLink;
+    }
+    if (prevLink != null) {
+      this.prev = nextLink;
+    }
+  }
+  
 }
 
 // Class for Sentinel node
 class Sentinel<T> extends ANode<T> {
-  
+
   // sentinel constructor, with a next and previous node
   public Sentinel() {
     this.next = this;
     this.prev = this;
+  }
+  
+  //
+  T remove() {
+    throw new RuntimeException("Deque is empty");
   }
 }
 
@@ -31,14 +50,33 @@ class Node<T> extends ANode<T> {
 
   // Convenience constructor for Node with data and links
   public Node(T data, ANode<T> next, ANode<T> prev) {
+    if (next == null || prev == null) {
+      throw new IllegalArgumentException("next and prev cannot null");
+    }
+    
     this.data = data;
     this.next = next;
     this.prev = prev;
-    if (next != null) {
-      next.prev = this;
+    
+    next.relink(null, this);
+    prev.relink(this, null); 
+  }
+  
+  //
+  T remove() {
+    this.prev = null;
+    this.next = null;
+    
+    return this.data;
+  }
+  
+  // 
+  void relink(Node<T> nextLink, Node<T> prevLink) {
+    if (nextLink != null) {
+      this.next = prevLink;
     }
-    if (prev != null) {
-      prev.next = this;
+    if (prevLink != null) {
+      this.prev = nextLink;
     }
   }
 }
@@ -51,7 +89,7 @@ class Deque<T> {
   public Deque() {
     this.header = new Sentinel<>();
   }
-  
+
   // Convenience constructor for Deque
   public Deque(Sentinel<T> header) {
     this.header = header;
@@ -59,51 +97,35 @@ class Deque<T> {
 
   // Method to get the size of the Deque
   public int size() {
-      int count = 0;
-      ANode<T> current = header.next;
-      while (current != header) {
-        count++;
-        current = current.next;
-      }
+    int count = 0;
+    ANode<T> current = header.next;
+    while (!current.equals(header)) {
+      count++;
+      current = current.next;
+    }
     return count;
   }
 
   // Method to add an item at the head
   public void addAtHead(T value) {
-      Node<T> newNode = new Node<>(value, header.next, header);
-      header.next = newNode;
-      if (newNode.next != header) {
-        newNode.next.prev = newNode;
-    }
+    Node<T> newNode = new Node<>(value, header.next, header);
   }
 
   // Method to add an item at the tail
   public void addAtTail(T value) {
-    Node<T> newNode = new Node<>(value, header, header.prev);
-    header.prev.next = newNode;
-    header.prev = newNode;
+    ANode<T> newNode = new Node<>(value, header, header.prev);
   }
 
   // Method to remove an item from the head
   public T removeFromHead() {
-    if (header.next == header) {
-      throw new RuntimeException("Deque is empty");
-    }
-    Node<T> toRemove = (Node<T>) header.next;
-    header.next = toRemove.next;
-    toRemove.next.prev = header;
-    return toRemove.data;
+    return header.next.remove();  
   }
 
-    // Method to remove an item from the tail
+  // Method to remove an item from the tail
   public T removeFromTail() {
-    if (header.prev == header) {
-      throw new RuntimeException("Deque is empty");
-    }
-    Node<T> toRemove = (Node<T>) header.prev;
-    header.prev = toRemove.prev;
-    toRemove.prev.next = header;
-    return toRemove.data;
+    //T removed = header.prev.removeFromTailHelper();
+    //header.prev = header.prev.prev;
+    return header.prev.remove();
   }
 
   // Method to find a node based on a predicate
@@ -115,7 +137,7 @@ class Deque<T> {
       }
       current = current.next;
     }
-    return header; // Return header if not found  
+    return header; // Return header if not found
   }
 }
 
@@ -127,13 +149,13 @@ class ExamplesDeques {
 
   // initializes all the deques
   void InitDeques() {
-    
-    //deque2 given examples
+
+    // deque2 given examples
     this.deque2.addAtTail("abc");
     this.deque2.addAtTail("bcd");
     this.deque2.addAtTail("cde");
     this.deque2.addAtTail("def");
-    
+
     // deque3 personal examples
     this.deque3.addAtTail("Zweihandler");
     this.deque3.addAtTail("Arbalest");
@@ -141,7 +163,7 @@ class ExamplesDeques {
     this.deque3.addAtTail("Caestus");
     this.deque3.addAtTail("Splitleaf GS");
   }
-  
+
   void ResetDeques() {
     this.deque1 = new Deque<>(); // Empty list
     this.deque2 = new Deque<>(); // List with values
@@ -149,14 +171,14 @@ class ExamplesDeques {
   }
 
   boolean testSize(Tester t) {
-    
+
     // resets the deque
     ResetDeques();
-    
+
     // initializes the deque nodes
     InitDeques();
-    
-    return t.checkExpect(this.deque1.size(), 0)
+
+    return t.checkExpect(this.deque1.size(), 0) 
         && t.checkExpect(this.deque2.size(), 4)
         && t.checkExpect(this.deque3.size(), 5);
   }
@@ -178,17 +200,35 @@ class ExamplesDeques {
     // check if size increased by one
     t.checkExpect(this.deque1.size(), 1);
   }
-  
+
 //  boolean testAddAtTail(Tester t) {
 //    System.out.println("Size of deque1: " + deque1.size()); // Should print 0  
 //    System.out.println("Size of deque2: " + deque2.size()); // Should print 4  
 //  }
-//
-//  boolean testRemoveFromHead(Tester t) {
-//    deque1.addAtHead("xyz");
-//    System.out.println("Removed from deque1: " + deque1.removeFromHead()); // Should print "xyz"  
-//    System.out.println("Size of deque1 after removal: " + deque1.size()); // Should print 0  
-//  }
+
+  boolean testRemoveFromHead(Tester t) {
+    
+    // resets the deque
+    ResetDeques();
+
+    // initializes the deque nodes
+    InitDeques();
+    
+    Deque<String> deque2Rh = new Deque<>();
+    
+    deque2Rh.addAtTail("bcd");
+    deque2Rh.addAtTail("cde");
+    deque2Rh.addAtTail("def");
+    
+    this.deque2.removeFromHead();
+    
+    return t.checkExpect(this.deque2, deque2Rh);
+    
+    //deque1.addAtHead("xyz");
+    //System.out.println("Removed from deque1: " + deque1.removeFromHead()); // Should print "xyz"  
+    //System.out.println("Size of deque1 after removal: " + deque1.size()); // Should print 0  
+  }
+
 //  boolean testRemoveFromTail(Tester t) {
 //    System.out.println("Size of deque1: " + deque1.size()); // Should print 0  
 //    System.out.println("Size of deque2: " + deque2.size()); // Should print 4  
