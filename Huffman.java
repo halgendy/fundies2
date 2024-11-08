bfd384d3abee098d822cd43a4936dd92f47ffac0
Hazem Algendy <156970407+halgendy@users.noreply.github.com>
2024-11-07 19:16:07 -0500

    Update Huffman.java

--- Huffman.java
+++ Huffman.java
@@ -75,10 +75,10 @@ class Huffman {
       String binaryCode = "";
 
       // get the code of the given char
-      binaryCode = this.root.getCode(ch, "");
+      binaryCode = this.root.findCode(ch, "");
 
       // if it is null, return null;
-      if (binaryCode == null) {
+      if (binaryCode.length() == 0) {
         throw new IllegalArgumentException(
             "Tried to encode " + ch + " but that is not part of the language.");
       }
@@ -92,33 +92,40 @@ class Huffman {
     return ans;
   }
 
-  // decodes a given arraylist of boolean using the huffman code tree
+  // Decodes a given ArrayList of booleans using the Huffman code tree
   public String decode(ArrayList<Boolean> encoded) {
-    String ans = "";
-    TotalNode currentNode = this.root;
+    String decoded = "";
+    String remainingBits = "";
 
-    // depending on the true or false, use the letter on the left or right
-    for (int i = 0; i < encoded.size(); i++) {
-      if (!encoded.get(i)) {
-        currentNode = currentNode.zero;
+    // Convert the list of booleans to a string of '0' and '1'
+    for (Boolean bit : encoded) {
+      if (bit) {
+        remainingBits += "1";
       }
       else {
-        currentNode = currentNode.one;
-      }
-
-      // if the node has a letter, add it to the answer then reset the current node
-      if (currentNode.letter != null) {
-        ans += currentNode.letter;
-        currentNode = this.root;
-      }
-
-      // if we are in a node that is outside the tree, add the question mark
-      if (currentNode != null && currentNode.letter == null && i == encoded.size() - 1
-          && !currentNode.equals(this.root)) {
-        ans += '?';
+        remainingBits += "0";
       }
     }
-    return ans;
+
+    // Decode the bit string one character at a time using findChar
+    while (remainingBits.length() != 0) {
+      String decodedChar = this.root.findChar(remainingBits);
+
+      if (decodedChar.length() == 0) {
+        // If findChar returns an empty string, we have leftover bits that do not decode
+        // to a character
+        decoded += "?";
+        break;
+      }
+      else {
+        // Append the decoded character and remove the corresponding bits
+        decoded += decodedChar;
+        remainingBits = remainingBits
+            .substring(this.root.findCode(decodedChar.charAt(0), "").length());
+      }
+    }
+
+    return decoded;
   }
 
   // insertion sort for a arraylist of nodes
@@ -126,15 +133,15 @@ class Huffman {
     for (int i = 1; i < tree.size(); i++) {
       INode key = tree.get(i);
       int j = i - 1;
-      
+
       // get the index of where the key node is the smallest
       while (j >= 0 && tree.get(j).greaterThan(key)) {
         j--;
       }
-      
+
       // remove the key
       tree.remove(i);
-      
+
       // add the key back in the new location
       tree.add(j + 1, key);
     }
@@ -142,19 +149,30 @@ class Huffman {
 }
 
 interface INode {
+
+  //
   Integer combineFreq(INode that);
-  
-  Integer combineFreqLeaf(LeafNode thatLeaf);
-  
-  Integer combineFreqTotal(TotalNode thatLeaf);
-  
-  String getCode(char ch, String ans);
-  
+
+  //
+  Integer combineFreq(LeafNode thatLeaf);
+
+  //
+  Integer combineFreq(TotalNode thatLeaf);
+
+  //
+  String findCode(char ch, String ans);
+
+  //
+  String findChar(String codeLeft);
+
+  //
   Boolean greaterThan(INode that);
-  
-  Boolean greaterThanLeaf(LeafNode that);
-  
-  Boolean greaterThanTotal(TotalNode that);
+
+  //
+  Boolean greaterThan(LeafNode that);
+
+  //
+  Boolean greaterThan(TotalNode that);
 }
 
 // represents a single node or tree of nodes
@@ -168,37 +186,47 @@ class LeafNode implements INode {
     this.freq = freq;
   }
 
+  //
   public Integer combineFreq(INode that) {
-    return that.combineFreqLeaf(this);
+    return that.combineFreq(this);
   }
-    
-  public Integer combineFreqLeaf(LeafNode thatLeaf) {
+
+  //
+  public Integer combineFreq(LeafNode thatLeaf) {
     return this.freq + thatLeaf.freq;
   }
-  
-  public Integer combineFreqTotal(TotalNode thatTotal) {
+
+  //
+  public Integer combineFreq(TotalNode thatTotal) {
     return this.freq + thatTotal.left.combineFreq(thatTotal.right);
   }
-  
+
   // a depth first search for char c
-  public String getCode(char ch, String ans) {
+  public String findCode(char ch, String codeAcc) {
 
     // if the node contains the letter, return the code finished code
     if (this.letter.charAt(0) == ch) {
-      return ans;
+      return codeAcc;
     }
     return "";
   }
-  
-  public Boolean greaterThan(INode that) {
-    return that.greaterThanLeaf(this);
+
+  public String findChar(String codeLeft) {
+    return this.letter;
   }
-  
-  public Boolean greaterThanLeaf(LeafNode that) {
+
+  // REPRESENTS
+  public Boolean greaterThan(INode that) {
+    return that.greaterThan(this);
+  }
+
+  // REPRESENTS
+  public Boolean greaterThan(LeafNode that) {
     return this.freq < that.freq;
   }
-  
-  public Boolean greaterThanTotal(TotalNode that) {
+
+  // REPRESENTS
+  public Boolean greaterThan(TotalNode that) {
     return this.freq < that.freq;
   }
 }
@@ -216,33 +244,66 @@ class TotalNode implements INode {
     this.freq = left.combineFreq(right);
   }
 
+  // REPRESENTS
   public Integer combineFreq(INode that) {
-    return that.combineFreqTotal(this);
+    return that.combineFreq(this);
   }
 
-  public Integer combineFreqLeaf(LeafNode thatLeaf) {
-    return this.freq ;
+  // REPRESENTS
+  public Integer combineFreq(LeafNode thatLeaf) {
+    return this.freq;
   }
-  
-  public Integer combineFreqTotal(TotalNode thatTotal) {
+
+  // REPRESENTS
+  public Integer combineFreq(TotalNode thatTotal) {
     return this.left.combineFreq(this.right) + thatTotal.left.combineFreq(thatTotal.right);
   }
-  
+
   // a depth first search for char c
-  public String getCode(char ch, String ans) {
+  public String findCode(char ch, String ans) {
     // add 0 to accumulated answer if left and 1 if going right
-    return this.left.getCode(ch, ans + '0') + this.right.getCode(ch, ans + '1');
+    String left = this.left.findCode(ch, ans + "0");
+    String right = this.right.findCode(ch, ans + "1");
+
+    if (left.length() == 0) {
+      return right;
+    }
+    else {
+      return left;
+    }
+
+    // return this.left.findCode(ch, ans + '0') + this.right.findCode(ch, ans +
+    // '1');
   }
-  
+
+  //
+  public String findChar(String codeLeft) {
+    if (codeLeft.length() == 0) {
+      return "";
+    }
+
+    String nextCode = codeLeft.substring(1);
+
+    if (codeLeft.charAt(0) == '0') {
+      return this.left.findChar(nextCode);
+    }
+    else {
+      return this.right.findChar(nextCode);
+    }
+  }
+
+  //
   public Boolean greaterThan(INode that) {
-    return that.greaterThanTotal(this);
+    return that.greaterThan(this);
   }
-  
-  public Boolean greaterThanLeaf(LeafNode that) {
+
+  //
+  public Boolean greaterThan(LeafNode that) {
     return this.freq < that.freq;
   }
-  
-  public Boolean greaterThanTotal(TotalNode that) {
+
+  //
+  public Boolean greaterThan(TotalNode that) {
     return this.freq < that.freq;
   }
 }
@@ -259,10 +320,12 @@ class ExamplesHuffman {
   ArrayList<Integer> intArr2 = new ArrayList<>(Arrays.asList(45, 45, 5, 13, 9, 9));
 
   // examples with an arraylist of nodes
-  ArrayList<INode> nodeArr1 = new ArrayList<INode>(Arrays.asList(new LeafNode("a", 12), new LeafNode("b", 45),
-      new LeafNode("c", 5), new LeafNode("d", 13), new LeafNode("e", 9), new LeafNode("f", 16)));
-  ArrayList<INode> nodeArr2 = new ArrayList<INode>(Arrays.asList(new LeafNode("a", 45), new LeafNode("b", 45),
-      new LeafNode("c", 5), new LeafNode("e", 13), new LeafNode("e", 9), new LeafNode("f", 9)));
+  ArrayList<INode> nodeArr1 = new ArrayList<INode>(
+      Arrays.asList(new LeafNode("a", 12), new LeafNode("b", 45), new LeafNode("c", 5),
+          new LeafNode("d", 13), new LeafNode("e", 9), new LeafNode("f", 16)));
+  ArrayList<INode> nodeArr2 = new ArrayList<INode>(
+      Arrays.asList(new LeafNode("a", 45), new LeafNode("b", 45), new LeafNode("c", 5),
+          new LeafNode("e", 13), new LeafNode("e", 9), new LeafNode("f", 9)));
 
   // huffmans for both examples
   Huffman h1;
@@ -318,12 +381,14 @@ class ExamplesHuffman {
 
     // replicate the h1 and h2 tree
     TotalNode repH1 = new TotalNode(new LeafNode("b", 45),
-        new TotalNode(new TotalNode(new LeafNode("a", 12), new LeafNode("d", 13)),
-            new TotalNode(new TotalNode(new LeafNode("c", 5), new LeafNode("e", 9)), new LeafNode("f", 16))));
-    
+        new TotalNode(new TotalNode(new LeafNode("a", 12), new LeafNode("d", 13)), new TotalNode(
+            new TotalNode(new LeafNode("c", 5), new LeafNode("e", 9)), new LeafNode("f", 16))));
+
     TotalNode repH2 = new TotalNode(new LeafNode("b", 45),
-        new TotalNode(new TotalNode(new TotalNode(new LeafNode("c", 5), new LeafNode("e", 9)),
-            new TotalNode(new LeafNode("f", 9), new LeafNode("e", 13))), new LeafNode("a", 45)));
+        new TotalNode(
+            new TotalNode(new TotalNode(new LeafNode("c", 5), new LeafNode("e", 9)),
+                new TotalNode(new LeafNode("f", 9), new LeafNode("e", 13))),
+            new LeafNode("a", 45)));
 
     // test if new root has expected structure and values
     t.checkExpect(this.h1.root, repH1);
@@ -342,27 +407,31 @@ class ExamplesHuffman {
 
     // test unsorted nodes
     t.checkExpect(this.nodeArr1,
-        new ArrayList<LeafNode>(Arrays.asList(new LeafNode("a", 12), new LeafNode("b", 45), new LeafNode("c", 5),
-            new LeafNode("d", 13), new LeafNode("e", 9), new LeafNode("f", 16))));
+        new ArrayList<LeafNode>(
+            Arrays.asList(new LeafNode("a", 12), new LeafNode("b", 45), new LeafNode("c", 5),
+                new LeafNode("d", 13), new LeafNode("e", 9), new LeafNode("f", 16))));
     t.checkExpect(this.nodeArr2,
-        new ArrayList<LeafNode>(Arrays.asList(new LeafNode("a", 45), new LeafNode("b", 45), new LeafNode("c", 5),
-            new LeafNode("e", 13), new LeafNode("e", 9), new LeafNode("f", 9))));
+        new ArrayList<LeafNode>(
+            Arrays.asList(new LeafNode("a", 45), new LeafNode("b", 45), new LeafNode("c", 5),
+                new LeafNode("e", 13), new LeafNode("e", 9), new LeafNode("f", 9))));
 
     // sort first array
     this.h1.iSort(this.nodeArr1);
 
     // test the sorted Arr1
     t.checkExpect(this.nodeArr1,
-        new ArrayList<LeafNode>(Arrays.asList(new LeafNode("c", 5), new LeafNode("e", 9), new LeafNode("a", 12),
-            new LeafNode("d", 13), new LeafNode("f", 16), new LeafNode("b", 45))));
+        new ArrayList<LeafNode>(
+            Arrays.asList(new LeafNode("c", 5), new LeafNode("e", 9), new LeafNode("a", 12),
+                new LeafNode("d", 13), new LeafNode("f", 16), new LeafNode("b", 45))));
 
     // sort second array
     this.h1.iSort(this.nodeArr2);
 
     // test the sorted Arr2
     t.checkExpect(this.nodeArr2,
-        new ArrayList<LeafNode>(Arrays.asList(new LeafNode("c", 5), new LeafNode("e", 9), new LeafNode("f", 9),
-            new LeafNode("e", 13), new LeafNode("a", 45), new LeafNode("b", 45))));
+        new ArrayList<LeafNode>(
+            Arrays.asList(new LeafNode("c", 5), new LeafNode("e", 9), new LeafNode("f", 9),
+                new LeafNode("e", 13), new LeafNode("a", 45), new LeafNode("b", 45))));
 
   }
 
@@ -381,8 +450,7 @@ class ExamplesHuffman {
 
     // test case with letter that isn't in nodes
     t.checkException(
-        new IllegalArgumentException(
-            "Tried to " + "encode z but that is not part of the language."),
+        new IllegalArgumentException("Tried to encode z but that is not part of the language."),
         this.h1, "encode", "abz");
 
     // test case with duplicate letters
@@ -397,8 +465,8 @@ class ExamplesHuffman {
                 false, false, true, false, true, true, true, false, true, true, true, true)));
   }
 
-  // test getCode method
-  void testGetCode(Tester t) {
+  // test findCode method
+  void testFindCode(Tester t) {
 
     // reset examples
     reset();
@@ -407,21 +475,47 @@ class ExamplesHuffman {
     init();
 
     // test all letters in h1
-    t.checkExpect(this.h1.getCode(this.h1.root, 'a', ""), "100");
-    t.checkExpect(this.h1.getCode(this.h1.root, 'b', ""), "0");
-    t.checkExpect(this.h1.getCode(this.h1.root, 'c', ""), "1100");
-    t.checkExpect(this.h1.getCode(this.h1.root, 'd', ""), "101");
-    t.checkExpect(this.h1.getCode(this.h1.root, 'e', ""), "1101");
-    t.checkExpect(this.h1.getCode(this.h1.root, 'f', ""), "111");
+    t.checkExpect(this.h1.root.findCode('a', ""), "100");
+    t.checkExpect(this.h1.root.findCode('b', ""), "0");
+    t.checkExpect(this.h1.root.findCode('c', ""), "1100");
+    t.checkExpect(this.h1.root.findCode('d', ""), "101");
+    t.checkExpect(this.h1.root.findCode('e', ""), "1101");
+    t.checkExpect(this.h1.root.findCode('f', ""), "111");
 
     // test all letters in h2
-    t.checkExpect(this.h2.getCode(this.h2.root, 'a', ""), "11");
-    t.checkExpect(this.h2.getCode(this.h2.root, 'b', ""), "0");
-    t.checkExpect(this.h2.getCode(this.h2.root, 'c', ""), "1000");
-    t.checkExpect(this.h2.getCode(this.h2.root, 'e', ""), "1001");
-    t.checkExpect(this.h2.getCode(this.h2.root, 'f', ""), "1010");
+    t.checkExpect(this.h2.root.findCode('a', ""), "11");
+    t.checkExpect(this.h2.root.findCode('b', ""), "0");
+    t.checkExpect(this.h2.root.findCode('c', ""), "1000");
+    t.checkExpect(this.h2.root.findCode('e', ""), "1001");
+    t.checkExpect(this.h2.root.findCode('f', ""), "1010");
 
   }
+  
+//test findCode method
+ void testFindChar(Tester t) {
+
+   // reset examples
+   reset();
+
+   // initialize examples
+   init();
+
+   // test all letters in h1
+   t.checkExpect(this.h1.root.findChar("100"), "a");
+   t.checkExpect(this.h1.root.findChar("0"), "b");
+   t.checkExpect(this.h1.root.findChar("1100"), "c");
+   t.checkExpect(this.h1.root.findChar("101"), "d");
+   t.checkExpect(this.h1.root.findChar("1101"), "e");
+   t.checkExpect(this.h1.root.findChar("111"), "f");
+
+   // test all letters in h2
+   t.checkExpect(this.h2.root.findChar("11"), "a");
+   t.checkExpect(this.h2.root.findChar("0"), "b");
+   t.checkExpect(this.h2.root.findChar("1000"), "c");
+   t.checkExpect(this.h2.root.findChar("1001"), "e");
+   t.checkExpect(this.h2.root.findChar("1010"), "f");
+
+ }
 
   // test decode method
   void testDecode(Tester t) {
